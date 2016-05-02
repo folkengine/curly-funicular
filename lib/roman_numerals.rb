bdedac937ddaea2c73ee1b7e46c4d21f2fe611f6
POET <POET>
2016-05-02 13:47:42 -0400

    Removed double quotes

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,19 +1,19 @@
 class RomanNumerals
 
   NUMERALS = [
-      ["M", 1000],
-      ["CM", 900],
-      ["D", 500],
-      ["CD", 400],
-      ["C", 100],
-      ["XC", 90],
-      ["L", 50],
-      ["XL", 40],
-      ["X", 10],
-      ["IX", 9],
-      ["V", 5],
-      ["IV", 4],
-      ["I", 1]
+      ['M', 1000],
+      ['CM', 900],
+      ['D', 500],
+      ['CD', 400],
+      ['C', 100],
+      ['XC', 90],
+      ['L', 50],
+      ['XL', 40],
+      ['X', 10],
+      ['IX', 9],
+      ['V', 5],
+      ['IV', 4],
+      ['I', 1]
   ]
 
   def initialize(numeric)
@@ -32,7 +32,7 @@ class RomanNumerals
   end
 
   def to_numeral
-    result = ""
+    result = ''
     NUMERALS.each do |numeral, arabic|
       while @value >= arabic
         result += numeral
