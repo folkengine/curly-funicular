d0b533403766a22411aa88becf35e0a9022696b2
Chris <ignatz@gmail.com>
2016-05-02 12:55:53 -0400

    added cucumber tests

--- features/roman_numerals.feature
+++ features/roman_numerals.feature
@@ -3,3 +3,6 @@ Feature: Validating Roman Numeral Kata code
   I would like to verify that the Roman Numeral kata code works correctly.
 
   Scenario:
+    Given that I have the number 1
+    When I call to_number
+    Then I should get back I
--- features/step_definitions/step_1.rb
+++ features/step_definitions/step_1.rb
@@ -1,2 +1,13 @@
 require 'roman_numerals'
 
+Given(/^that I have the number (\d+)$/) do |integer|
+  @roman_numeral = RomanNumerals.new(integer)
+end
+
+When(/^I call to_number$/) do
+  @result = @roman_numeral.to_numeral
+end
+
+Then(/^I should get back (.*)/) do |roman_numeral|
+  @result.eql?(roman_numeral)
+end
